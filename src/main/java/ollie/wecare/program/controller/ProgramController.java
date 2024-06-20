345665a85e0c2adf9b3df5a7da24fd934ca35264
Haeun-Y <hahaha329@ewhain.net>
2024-06-21 08:05:07 +0900

    #3 feat: 프로그램 등록 API 구현

--- src/main/java/ollie/wecare/program/controller/ProgramController.java
+++ src/main/java/ollie/wecare/program/controller/ProgramController.java
@@ -1,11 +1,36 @@
 package ollie.wecare.program.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ollie.wecare.common.base.BaseException;
+import ollie.wecare.common.base.BaseResponse;
+import ollie.wecare.program.dto.PostProgramReq;
+import ollie.wecare.program.service.ProgramService;
+import org.springframework.web.bind.annotation.*;
+
+import static ollie.wecare.common.base.BaseResponseStatus.SUCCESS;
 
 @RestController
 @RequestMapping("/programs")
 @RequiredArgsConstructor
 public class ProgramController {
+    private final ProgramService programService;
+    /*
+     * 프로그램 조회 (월별)
+     * */
+    /*
+    @GetMapping("/{month}")
+    @ResponseBody
+    public BaseResponse<List<GetProgramRes>> getPrograms(HttpServletRequest request, @PathVariable Long month) {
+        return new BaseResponse<>(programService.getPrograms(month));
+    }*/
+
+    /*
+     * 프로그램 등록
+     * */
+    @PostMapping
+    @ResponseBody
+    public BaseResponse<String> saveProgram(@RequestBody PostProgramReq postProgramReq) throws BaseException {
+        programService.saveProgram(postProgramReq);
+        return new BaseResponse<>(SUCCESS);
+    }
 }
--- src/main/java/ollie/wecare/program/service/ProgramService.java
+++ src/main/java/ollie/wecare/program/service/ProgramService.java
@@ -1,9 +1,50 @@
 package ollie.wecare.program.service;
 
 import lombok.RequiredArgsConstructor;
+import ollie.wecare.challenge.entity.Challenge;
+import ollie.wecare.challenge.repository.ChallengeRepository;
+import ollie.wecare.common.base.BaseException;
+import ollie.wecare.common.enums.Role;
+import ollie.wecare.program.dto.GetProgramRes;
+import ollie.wecare.program.dto.PostProgramReq;
+import ollie.wecare.program.entity.Program;
+import ollie.wecare.program.repository.ProgramRepository;
+import ollie.wecare.user.entity.User;
+import ollie.wecare.user.repository.UserRepository;
+import ollie.wecare.user.service.AuthService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static ollie.wecare.common.base.BaseResponseStatus.INVALID_ROLE;
+import static ollie.wecare.common.base.BaseResponseStatus.INVALID_USER_IDX;
 
 @Service
 @RequiredArgsConstructor
 public class ProgramService {
+
+    private final AuthService authService;
+    private final ProgramRepository programRepository;
+    private final ChallengeRepository challengeRepository;
+
+    private final UserRepository userRepository;
+    public List<GetProgramRes> getPrograms(Long month) {
+        //programRepository.findByDueDate
+        return null;
+    }
+
+    public void saveProgram(PostProgramReq postProgramReq) throws BaseException {
+        User user = userRepository.findById(authService.getUserIdx()).orElseThrow(()-> new BaseException(INVALID_USER_IDX));
+        if(!user.getRole().equals(Role.Admin)) throw new BaseException(INVALID_ROLE);
+
+        Program program = PostProgramReq.toProgram(postProgramReq);
+        programRepository.save(program);
+
+        this.saveChallenge(PostProgramReq.toProgram(postProgramReq), user);
+    }
+
+    public void saveChallenge(Program program, User user) {
+        challengeRepository.save(Challenge.builder().program(program).name(program.getName()).attendanceRate(0L)
+                .admin(user).host(program.getHost()).totalNum(0L).build());
+    }
 }
