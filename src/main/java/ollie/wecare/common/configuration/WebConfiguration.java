1c77dd0fd909305cf9b2a05aaaf872279002663f
joonghyun <joongh@ewhain.net>
2024-06-27 22:06:34 +0900

    #28 fix: allowedOrigins에 배포 url 추가 수정

--- src/main/java/ollie/wecare/common/configuration/WebConfiguration.java
+++ src/main/java/ollie/wecare/common/configuration/WebConfiguration.java
@@ -10,7 +10,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://team-ollie.github.io/WeCare-FE")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://team-ollie.github.io/WeCare-FE", "https://team-ollie.github.io")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
